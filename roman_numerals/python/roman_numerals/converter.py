01bd93059a7b4d7b2c2b63a41616d408b6ddabff
Uttam Kini <uttamkini@gmail.com>
2018-10-23 14:29:02 +0530

    [Refactor roman_numerals] Use reversed string
    so that we eliminate one more if condition.
    Also remove the "class" from the test

--- roman_numerals/python/roman_numerals/converter.py
+++ roman_numerals/python/roman_numerals/converter.py
@@ -1,30 +1,28 @@
-from _ctypes import ArgumentError
-
 _single_letter_numeral_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 _subtractive_numerals = [1, 10, 100]
 
 
 def convert_to_decimal(roman):
-    return _decimal_value(roman, 0)
+    reversed_roman = roman[::-1]
+    return _decimal_value(reversed_roman, 0)
 
 
-def _decimal_value(roman, decimal):
-    numeral_length = len(roman)
-    if numeral_length == 0:
-        return decimal
-    elif numeral_length == 1:
-        return decimal + _decimal_value_1(roman[0])
-    elif numeral_length == 3:
-        return decimal + _decimal_value_1(roman[0]) + _decimal_value_2(roman[1], roman[2])
+def _decimal_value(roman, total_decimal):
+    roman_numeral_length = len(roman)
+    if roman_numeral_length == 0:
+        return total_decimal
+    elif roman_numeral_length == 1:
+        return total_decimal + _decimal_value_1(roman[0])
     else:
-        return _decimal_value(roman[2:], decimal + _decimal_value_2(roman[0], roman[1]))
+        new_total_decimal = total_decimal + _decimal_value_2(roman[1], roman[0])
+        return _decimal_value(roman[2:], new_total_decimal)
 
 
 def _decimal_value_1(roman):
     try:
         return _single_letter_numeral_map[roman]
     except KeyError:
-        raise ArgumentError("Invalid Roman Numeral: should be one of {0}".format(_single_letter_numeral_map))
+        raise ValueError("Invalid Roman Numeral: should be one of {0}".format(_single_letter_numeral_map))
 
 
 def _decimal_value_2(first, second):
@@ -35,7 +33,7 @@ def _decimal_value_2(first, second):
     elif _is_valid_subtraction(first_decimal, second_decimal):
         return second_decimal - first_decimal
     else:
-        raise ArgumentError(
+        raise ValueError(
             "Invalid Roman Numeral: only I, X and C are allowed as subtractive numerals"
             " and the following numeral must not be more than 100 times the first")
 
