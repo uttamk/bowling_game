5be6d42c157a38b0edcb9a586f8d90c3a7c603b0
Uttam Kini <uttamkini@gmail.com>
2018-10-23 11:41:10 +0530

    Revert "Refactor roman numerals into a more succint representation"
    This reverts commit 88a8094d60f60bb4d69c8e2173a3156cd10ba32c.

--- roman_numerals/python/roman_numerals/converter.py
+++ roman_numerals/python/roman_numerals/converter.py
@@ -1,9 +1,29 @@
-from functools import reduce
-
 _single_letter_numeral_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
 
 def convert_to_decimal(roman):
-    return reduce(
-        lambda total, letter: total + _single_letter_numeral_map[letter] - 2 * (
-                    total % _single_letter_numeral_map[letter]), roman, 0)
+    return _decimal_value(roman, 0)
+
+
+def _decimal_value(roman, decimal):
+    if len(roman) == 0:
+        return decimal
+    elif len(roman) == 1:
+        return decimal + _decimal_value_1(roman[0])
+    elif len(roman) == 3:
+        return decimal + _decimal_value_1(roman[0]) + _decimal_value_2(roman[1], roman[2])
+    else:
+        return _decimal_value(roman[2:], decimal + _decimal_value_2(roman[0], roman[1]))
+
+
+def _decimal_value_1(roman):
+    return _single_letter_numeral_map[roman]
+
+
+def _decimal_value_2(first, second):
+    first_decimal = _decimal_value_1(first)
+    second_decimal = _decimal_value_1(second)
+    if first_decimal >= second_decimal:
+        return first_decimal + second_decimal
+    else:
+        return second_decimal - first_decimal
